f848a3cdb77cd1f4ef5e6eceb947b426dbf3a8f4
Jared Tichacek <jared.tichacek@gmail.com>
2023-10-18 14:06:17 -0500

    implemented project list class and headers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // Validation
 
-interface Validatable {
+    interface Validatable {
     // all props except value are optional
     value: string | number
     required?: boolean
@@ -43,6 +43,35 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     }
    }  
    return adjDescriptor
+}
+
+// ProjectList Class
+
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+        
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${type}-projects`
+        this.attach()
+        this.renderContent()
+
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+    }
 }
 
 // ProjectInput Class
@@ -133,6 +162,8 @@ private configure() {
 }
 
 const prjInput = new ProjectInput()
+const activePrjList = new ProjectList('active')
+const finishedPrjList = new ProjectList('finished')
 // // Validation
 // interface Validatable {
 //     value: string | number
